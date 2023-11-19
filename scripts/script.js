35c4385536a74b4c7d5f7222d6d70f4ebf95115f
LucasBalbueno <balbuenolucas04@gmail.com>
2023-11-19 20:48:12 -0300

    criando as animações com Javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
-const lastOne = document.querySelector('#last-One');
-
 const myObserver = new IntersectionObserver((entries) => {
-    console.log(entries)
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting === true) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
 })
 
-myObserver.observe(lastOne)
+const elements = document.querySelectorAll('.hidden, .hidden2, .hidden3, .hidden4')
+
+elements.forEach((element) => myObserver.observe(element))
